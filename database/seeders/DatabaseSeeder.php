7a74fec8f22fe51becacd0d961880c763eea7c39
Adrian C. <11005331+adrianc111@users.noreply.github.com>
2024-11-17 16:14:59 +0000

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,8 +15,8 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         User::factory()->create([
-            'name'     => 'Adrian',
-            'email'    => 'adrian.cebanica@pm.me',
+            'name'     => 'Admin',
+            'email'    => 'admin@example.com',
             'password' => 'testtest',
         ]);
     }
